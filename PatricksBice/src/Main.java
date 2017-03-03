55a23b2d355ecc7e2501b8624ae9e202441eea84
Jim <jim_larsson_dev@hotmail.com>
2017-03-03 22:43:26 +0100

    Dags igen

--- PatricksBice/src/Main.java
+++ PatricksBice/src/Main.java
@@ -18,6 +18,7 @@ public class Main {
 			System.out.println("Fuel: " + v.fuelType);
 			System.out.println(""); //Lämna en tom rad
 			System.out.println("Bananus");
+			System.out.println("Korvas");
 			
 			if (v.getClass().equals(Truck.class))
 				System.out.println("CargoVolume: " + ((Truck) v).getCargoVolume());
